221c046b1e7cb2d1f84776a7f81fdb9dea37a1ab
Daniel Lenshin <daniel@izh46.com>
2021-01-25 16:53:04 -0500

    Added settings capability + SQL connectivity to AnketaDB

--- AnketaDB.java
+++ AnketaDB.java
@@ -1,8 +1,6 @@
 //Settings.json imports
 import java.io.File;
-import java.io.FileWriter;
 import java.io.FileReader;
-import java.io.Console;
 import java.io.IOException;
 import org.json.JSONObject;
 import org.json.JSONException;
@@ -21,13 +19,49 @@ import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JList;
+import java.awt.CardLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class AnketaDB extends JPanel
+public class AnketaDB extends JFrame
 {
-    String connectionAddress = "localhost"; //IP Address of the database
-    String username = "";
+    File settingsFile; //File object for settings.json
+    FileReader settingsReader; //FileReader object to read settingsFile
+    String settingsFileString; //String which contains the contents of settingsFile
+    JSONObject settings; //JSONObject which contains a JSONObject of the data in settingsFile
+
+    String username; //String which contains MariaDB user username
+    String password; //String which contains MariaDB user password
+    String address; //String which contains MariaDB server address\
+    
+    Connection databaseConnection; //Connection object which stores the connection to the SQL server
+    Statement statement; //Statement object which stores statements to the SQL server
+    ResultSet results; //ResultSet object which stores the results of SQL queries
+
+    public AnketaDB() throws IOException, SQLException
+    {
+        settingsFile = new File("settings.json"); //Sets settingsFile as settings.json
+        settingsReader = new FileReader(settingsFile); //Set settingsReader to read from settingsFile
+        while(settingsReader.ready()) //Sets settingsFileString as the content in settingsFile (formatted to remove tabs, spaces, and newlines)
+        {
+            settingsFileString += Character.toString(settingsReader.read());
+        }
+        settings = new JSONObject(settingsFileString.replaceAll("[\n\t ]", "")); //Sets settings to a JSONObject with the contents of settingsFileString
+
+        //Sets username, password, and address equal to their counterparts in settings JSONobject
+        username = settings.getString("username");
+        password = settings.getString("password");
+        address = settings.getString("address");
+
+        databaseConnection = DriverManager.getConnection(("jdbc:mysql://" + address), username, password);
+
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
