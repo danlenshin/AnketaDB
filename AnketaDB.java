9905f52699442dadf6e2a5b6195bef82ffd232d6
Daniel Lenshin <daniel@izh46.com>
2021-01-27 00:18:59 -0500

    got a window up yessirrrrr

--- AnketaDB.java
+++ AnketaDB.java
@@ -19,9 +19,8 @@ import javax.swing.JLabel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JList;
+import java.awt.Container;
 import java.awt.CardLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -32,13 +31,16 @@ public class AnketaDB extends JFrame
     String settingsFileString; //String which contains the contents of settingsFile
     JSONObject settings; //JSONObject which contains a JSONObject of the data in settingsFile
 
-    String username; //String which contains MariaDB user username
-    String password; //String which contains MariaDB user password
-    String address; //String which contains MariaDB server address\
+    String username; //String which contains SQL server user username
+    String password; //String which contains SQL server user password
+    String address; //String which contains SQL server address
     
     Connection databaseConnection; //Connection object which stores the connection to the SQL server
     Statement statement; //Statement object which stores statements to the SQL server
     ResultSet results; //ResultSet object which stores the results of SQL queries
+
+    CardLayout cards; //CardLayout object for creating a card layout
+    Container container; //Container object which the card layout will be on
 
     public AnketaDB() throws IOException, SQLException
     {
@@ -48,20 +50,48 @@ public class AnketaDB extends JFrame
         {
             settingsFileString += Character.toString(settingsReader.read());
         }
-        settings = new JSONObject(settingsFileString.replaceAll("[\n\t ]", "")); //Sets settings to a JSONObject with the contents of settingsFileString
+        settingsFileString = settingsFileString.replaceAll("null", ""); //Removes the initial null character read from settings.json, which is "null" in the settingsFileString
+        settings = new JSONObject(settingsFileString.replaceAll("[^\\S]", "")); //Sets settings to a JSONObject with the contents of settingsFileString
 
         //Sets username, password, and address equal to their counterparts in settings JSONobject
         username = settings.getString("username");
         password = settings.getString("password");
         address = settings.getString("address");
 
-        databaseConnection = DriverManager.getConnection(("jdbc:mysql://" + address), username, password);
+        databaseConnection = DriverManager.getConnection(("jdbc:mysql://" + address), username, password); //Sets databaseConnection as the connection to the SQL server
 
+        cards = new CardLayout(); //Set cards to a new CardLayout object
+        container = getContentPane(); //Sets the container to the content pane
+        container.setLayout(cards); //Sets the layout of the container to cards
+        setResizable(false); //Disables the user from resizing the JFrame
+        setSize(800, 600); //Sets the size of the JFrame to 800 x 600 pixels
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Sets it so that the program will close every time the frame is closed
 
+        /*
+        Creating the main panel and adding it to cards
+        */
+        JPanel main = new JPanel();
+        main.setLayout(null);
+
+        JButton mainCreateSurveyButton = new JButton("Создать Анкету");
+        mainCreateSurveyButton.setBounds(100, 50, 250, 50);
+        main.add(mainCreateSurveyButton);
+
+        JButton mainListOfSurveysButton = new JButton("Список Анкет");
+        mainListOfSurveysButton.setBounds(350, 50, 250, 50);
+        main.add(mainCreateSurveyButton);
+
+        //!TEST PORTION
+        container.add("main", main);
+        cards.show(container, "main");
+        //!TEST PORTION
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, SQLException
     {
-
+        //!TEST PORTION
+        JFrame frame = new AnketaDB();
+        frame.setVisible(true);
+        //!TEST PORTION
     }
 }
